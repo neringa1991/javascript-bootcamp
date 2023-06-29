aea8334a6995e2b72e85717493ff2bc3205d5ddf
neringa1991 <neringa1991@yahoo.com>
2023-06-29 15:32:41 +0300

    feat: add mongodb task code using query methods from mongodb

--- mongodb/controllers/userController.js
+++ mongodb/controllers/userController.js
@@ -1,94 +1,44 @@
 import userModel from "../models/userModel.js";
 
-// add user. req and res are comming from express
-export const createUser = async (req, res) => {
+// Task for today:
+// Use existing NoSQL DB that you have built already and implement on top of it some additional routes using following query methods from mongoose:
+
+// count: Returns the count of documents that match the specified criteria.
+// sort: Sorts the retrieved documents based on a specified field or fields.
+// limit: Limits the number of retrieved documents.
+
+// count: Returns the count of documents that match the specified criteria.
+export const countUsers = async (req, res) => {
   try {
-    const newUser = new userModel({
-      ...req.body,
-    });
-
-    //below is the same as above
-    // const newUser = new userModel({
-    //   username: req.body.userName,
-    //   password: req.body.password,
-    // });
-    await newUser.save();
-
-    const {password, ...remainingUserData} = newUser._doc;
-
-    // if we don't send status, the route will not be resolved
-    res.status(201).json(remainingUserData);
-    // res.status(201).send(`New user username: ${req.body.username} is created`);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(error);
-  }
-};
-// get all users
-export const getAllUsers = async (req, res) => {
-  try {
-    // password:0 means we don't want to send back the passwords from the database
-    const users = await userModel.find({}, {password: 0});
-
-    res.status(201).send(users);
-  } catch (error) {
-    console.log(error);
-
-    res.status(400).send(error);
-  }
-};
-// get one user
-export const getUser = async (req, res) => {
-  try {
-    // {password: 0} is built in mongoose method which removes password from being sent back
-    const user = await userModel.find(
-      {username: req.params.username},
-      {password: 0}
-    );
-
-    // below is the same above
-    // const user = await userModel.findOne({username: req.body.username});
-
-    res.status(201).send(user);
-  } catch (error) {
-    console.log(error);
-
-    res.status(400).send(error);
-  }
-};
-// delete student
-export const deleteUser = async (req, res) => {
-  try {
-    const user = await userModel.deleteOne(
-      {username: req.params.username},
-      {password: 0}
-    );
-
-    // const {password, ...remainingUserData} = user._doc;
-    res.status(201).send(user);
+    const usersCount = await userModel.countDocuments({username: "Aleksas2"});
+    res.status(201).json(usersCount);
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
   }
 };
 
-// update
-export const updateUser = async (req, res) => {
+// sort: Sorts the retrieved documents based on a specified field or fields.
+export const sortUsers = async (req, res) => {
   try {
-    const user = await userModel.findOneAndUpdate(
-      {
-        username: req.params.username,
-      },
-      {
-        $set: req.body,
-      },
-      {new: true}
-    );
+    const sortedUsers = await userModel
+      .find({}, {password: 0})
+      .sort({username: "asc"});
 
-    res.status(201).send(user);
+    res.status(201).send(sortedUsers);
   } catch (error) {
     console.log(error);
+    res.status(400).send(error);
+  }
+};
 
+//limit: Limits the number of retrieved documents.
+export const limitUsers = async (req, res) => {
+  try {
+    const limitedUsers = await userModel.find({}, {password: 0}).limit(3);
+    res.status(201).send(limitedUsers);
+  } catch (error) {
+    console.log(error);
     res.status(400).send(error);
   }
 };
--- mongodb/routes/userRoute.js
+++ mongodb/routes/userRoute.js
@@ -1,40 +1,14 @@
 import express from "express";
 
 import {
-  createUser,
-  getAllUsers,
-  getUser,
-  deleteUser,
-  updateUser,
+  countUsers,
+  sortUsers,
+  limitUsers,
 } from "../controllers/userController.js";
 
 const router = express.Router();
-
-router.post("/create", createUser);
-
-// below is the same as above
-// router.post("/create", async (req, res) => {
-//   try {
-//     const newUser = new userModel({
-//       ...req.body,
-//     });
-
-//     await newUser.save();
-
-//     // if we don't send status, the route will not be resolved
-//     res.status(201).send(`New user username: ${req.body.username} is created`);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400).send(error);
-//   }
-// });
-
-router.get("/get-all", getAllUsers);
-router.get("/get/:username", getUser);
-router.delete("/delete/:username", deleteUser);
-router.put("/update/:username", updateUser);
-
-// params is below and body is url
-//req.params.name123
+router.get("/get-count", countUsers);
+router.get("/get-sorted", sortUsers);
+router.get("/get-limit", limitUsers);
 
 export default router;
